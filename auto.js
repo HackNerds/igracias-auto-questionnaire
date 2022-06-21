cf5b4d55188e6935b3cadd092dc78dd9fc01709f
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2022-06-21 23:53:57 +0700

    add file auto.js

---
+++ auto.js
@@ -0,0 +1,8 @@
+const options = ['Sangat puas', 'Ya', 'Sangat setuju'];
+[...document.querySelectorAll('.answerlist1')].forEach((answer) => {
+  if (options.includes(answer.innerText)) {
+    answer.parentElement
+      .getElementsByClassName('answerlist2')[0]
+      .children[0].click();
+  }
+});
